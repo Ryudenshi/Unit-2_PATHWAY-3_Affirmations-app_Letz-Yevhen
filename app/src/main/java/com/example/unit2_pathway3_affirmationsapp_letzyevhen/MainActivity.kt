d092052834c4c237c1d56d3b1f5b02be799d7254
Ryudenshi <skarydog17@gmail.com>
2023-11-10 10:15:05 +0200

    RecicleView added

--- app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/MainActivity.kt
+++ app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/MainActivity.kt
@@ -9,12 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textview)
-
-        // Create a Datasource and load affirmations
-        val affirmationsList = Datasource().loadAffirmations()
-
-        // Display the size of the affirmations list in the TextView
-        textView.text = affirmationsList.size.toString()
     }
 }
