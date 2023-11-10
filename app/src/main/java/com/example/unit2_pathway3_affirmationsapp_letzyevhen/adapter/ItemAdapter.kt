65ad3a7f9be5d6d430a1fd25017bf96a7787f1c6
Ryudenshi <skarydog17@gmail.com>
2023-11-10 10:25:20 +0200

    ItemAdapter modified

--- app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/adapter/ItemAdapter.kt
@@ -1,8 +1,31 @@
 package com.example.unit2_pathway3_affirmationsapp_letzyevhen.adapter
 
 import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.unit2_pathway3_affirmationsapp_letzyevhen.R
 import com.example.unit2_pathway3_affirmationsapp_letzyevhen.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
-    class ItemViewHolder()
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
