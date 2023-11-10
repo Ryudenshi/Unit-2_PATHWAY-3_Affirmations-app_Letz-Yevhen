c849ff9c940256072223cb6ae74f88436c83fbbd
Ryudenshi <skarydog17@gmail.com>
2023-11-10 09:25:46 +0200

    First commit

---
+++ app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unit2_pathway3_affirmationsapp_letzyevhen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
