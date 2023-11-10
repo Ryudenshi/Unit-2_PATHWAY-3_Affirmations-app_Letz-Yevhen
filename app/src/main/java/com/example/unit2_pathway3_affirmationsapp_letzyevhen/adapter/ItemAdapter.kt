1ac9b53e0089deb2429f4a7230f89b0906ce55a3
Ryudenshi <skarydog17@gmail.com>
2023-11-10 10:20:45 +0200

    ItemAdapter added and modified

---
+++ app/src/main/java/com/example/unit2_pathway3_affirmationsapp_letzyevhen/adapter/ItemAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.unit2_pathway3_affirmationsapp_letzyevhen.adapter
+
+import android.content.Context
+import com.example.unit2_pathway3_affirmationsapp_letzyevhen.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+    class ItemViewHolder()
+}
